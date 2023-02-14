ab4465bd8ca39d0e94c8d4964d7d1af6c514c5b6
Mac Nisbet <mfnisbet@lakeheadu.ca>
2023-02-14 18:27:12 -0500

    Call API attempt
    Tried calling the API after pressing a button, but getting 401 (malformed) erorr

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
 
 function App() {
@@ -7,19 +9,28 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test React App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+        <Button>styled Button</Button>
+        </Container>
       </header>
     </div>
   );
 }
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+
+`;
+
+const Container = styled.div`
+  text-align: center;
+`
 
 export default App;
