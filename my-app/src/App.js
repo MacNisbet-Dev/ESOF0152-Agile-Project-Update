4e718f2270a1f347862b37edd521f72dcebed3e1
Mac Nisbet <mfnisbet@lakeheadu.ca>
2023-02-28 20:04:44 -0500

    Added Fetcher
    Added a demo/starting point to accessing the API. Note: can only currently be seen in the console.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import styled from 'styled-components'
 import './App.css';
 
+import axios from "axios";
+
+const options = {
+  method: 'GET',
+  url: 'https://edamam-food-and-grocery-database.p.rapidapi.com/parser',
+  params: {ingr: 'apple'},
+  headers: {
+    'X-RapidAPI-Key': '7692bae7cfmshac9d83a53f8151bp19f475jsn305a0583021f',
+    'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
 function App() {
   return (
     <div className="App">
