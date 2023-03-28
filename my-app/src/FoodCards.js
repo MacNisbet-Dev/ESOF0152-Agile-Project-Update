e25d01ad3a53d26385dfde5980e03946c88006fe
Mac Nisbet <mfnisbet@lakeheadu.ca>
2023-03-28 18:22:25 -0400

    Updated Food cards to tables
    Updated the food cards component to Sravya's tables. Couldn't figure out how to put the measures list in a table since there was too much text and it would overflow in the column.

--- my-app/src/FoodCards.js
+++ my-app/src/FoodCards.js
@@ -26,25 +26,47 @@ export function FoodCards({ FoodResponseData, amount, onSelectLabel }) {
                   'https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg'
                 }
                 alt={hint.food.label}
-              />
-              <CardText> Calories: {Math.round(amount * hint.food.nutrients.ENERC_KCAL * 100) / 100} kcal </CardText>
-              <CardText>Fat: {Math.round(amount * hint.food.nutrients.FAT * 100) / 100} grams</CardText>
-              <CardText>Protein: {Math.round(amount * hint.food.nutrients.PROCNT * 100) / 100} grams</CardText>
-              <CardText>Carbs: {Math.round(amount * hint.food.nutrients.CHOCDF * 100) / 100} grams</CardText>
+          />
+          <CardText>
+          <table>
+            <thead>
+                  <tr>
+                    <th>Nutrient</th>
+                    <th>Amount</th>
+                  </tr>
+            </thead>
+              <tbody>
+                <tr>
+                  <td>Calories</td>
+                  <td>{Math.round(amount * hint.food.nutrients.ENERC_KCAL * 100) / 100} kcal</td>
+                </tr>
+                <tr>
+                  <td>Fat</td>
+                  <td>{Math.round(amount * hint.food.nutrients.FAT * 100) / 100} grams</td>
+                </tr>
+                <tr>
+                  <td>Protein</td>
+                  <td>{Math.round(amount * hint.food.nutrients.PROCNT * 100) / 100} grams</td>
+                </tr>
+                <tr>
+                  <td>Carbs</td>
+                  <td>{Math.round(amount * hint.food.nutrients.CHOCDF * 100) / 100} grams</td>
+                </tr>
+              </tbody>
+              </table>
               <Button onClick={handleLabelSelect}>Search Recipes</Button>
-              <ul>
-                {measuresWithQuantity.map((measure, index) => (
-                  <li key={index}>
-                    {measure.label}: {measure.quantity} {measure.unit}
-                  </li>
-                ))}
-              </ul>
-
-            </RecipeCard>
-          </CardContainer>
-        );
-      })}
-    </div>
+              <CardMeasure>Serving Quantity Measure</CardMeasure>
+                    {measuresWithQuantity.map((measure, index) => (
+                              <li key={index}>
+                                {measure.label}: {measure.quantity} {measure.unit}
+                              </li>
+                            ))}
+                  </CardText>
+                </RecipeCard>
+              </CardContainer> 
+          );
+        })}
+     </div>
   );
 }
 
@@ -57,7 +79,11 @@ const CardHeader = styled.header`
 
 const CardText = styled.p`
   font-size: 20px;
-  padding-left: 30px;
+  padding-left: 25px;
+`
+const CardMeasure = styled.p`
+  font-size: 20px;
+  font-weight: bold;
 `
 
 const CardContainer = styled.div`
@@ -90,9 +116,10 @@ const Button = styled.button`
   background-color: black;
   color: white;
   font-size: 20px;
-  padding: 10px 10px;
+  padding: 5px 10px;
   border-radius: 5px;
   cursor: pointer;
   width: 200px;
   margin-left: 50px;
+  margin-top: 10px;
 `;
