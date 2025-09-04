9805e48a1c333b7fc39e469ebc32453c6de08c1f
Mac Nisbet <macnisbetdev@gmail.com>
2025-09-04 15:34:39 -0400

    Removed old functions
    Removed unused functions that were moved to the netlify server functions. Also removed comments for old functions.

--- my-app/src/APIFood.js
+++ my-app/src/APIFood.js
@@ -1,34 +1,7 @@
 /*
-* Create options makes a GET request from the edamam API
-* ingr: value is the search value, the categories can be used to filter
-* RapidAPI is the service used to make the call, the key and host header is required
-* Internal function, used by the makeFoodRequests function
-*/
-
-// function createOptions(value){
-//   const Options = {
-//     method: 'GET',
-//     url: 'https://edamam-food-and-grocery-database.p.rapidapi.com/api/food-database/v2/parser',
-//     params: {
-//       app_id: process.env.REACT_APP_EDAMAM_API_ID,
-//       app_key: process.env.REACT_APP_EDAMAM_API_KEY,
-//       ingr: value,
-//       'category[0]': 'generic-foods',
-//       'health[0]': 'alcohol-free',
-//       'nutrition-type': 'cooking'
-//     },
-//     headers: {
-//       "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY,
-//       "X-RapidAPI-Host": process.env.REACT_APP_RAPIDAPI_HOST_EDAMAM
-//     }
-//   };
-//   return Options;
-// }
-/*
 * makeFoodRequest is a public function that calls createOptions to make an array of hints with the request
 * response.data holds the array of hints which is all the values of the API
-* Automatic error handling, prints to console on error
-* Returns the response.data to the app.js component
+* Uses the netlify server functions
 */
 export function makeFoodRequest(value) {
   return fetch("/.netlify/functions/foodRequest", {
--- my-app/src/APIRecipe.js
+++ my-app/src/APIRecipe.js
@@ -1,33 +1,6 @@
 /*
-Create options makes a GET request from the Tasty API via RapidAPI
-q: the search query
-from/size: Page controls
-tags: optional filters (like "under_30_minutes")
-Dropped in favour of normalizing the data 
-*/
-// function createRecipeOptions(value) {
-//   const options = {
-//     method: "GET",
-//     url: "https://tasty.p.rapidapi.com/recipes/list",
-//     params: {
-//       q: value,
-//       from: 0,
-//       size: 20,
-//       tags: "under_30_minutes"
-//     },
-//     headers: {
-//       "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY,
-//       "X-RapidAPI-Host": process.env.REACT_APP_RAPIDAPI_HOST_TASTY
-//     }
-//   };
-//   return options;
-// }
-
-
-/*
 * makeRecipeRequest calls createRecipeOptions and returns the API's response data
-* response.data.results is the array of recipes from Tasty
-* errors are logged and re-thrown for handling in the UI
+* Uses the netlify functions
 */
 export async function makeRecipeRequest(value) {
   try {
@@ -46,31 +19,4 @@ export async function makeRecipeRequest(value) {
     console.error("Tasty API error:", error);
     throw new Error(`Unable to make recipe request: ${error.message}`);
   }
-}
-
-/*
-* Normalize the response from the tasty API
-* If another API has to be used for whatever reason write another normalizer function and use the same schema
-* Saves some headache swapping between APIs 
-*/
-
-function normalizeTastyRecipe(recipe) {
-  return {
-    id: recipe.id?.toString() || recipe.canonical_id || "",
-    title: recipe.name || "Untitled recipe",
-    image: recipe.thumbnail_url || recipe.beauty_url || "",
-    description: recipe.description || "",
-    ingredients: recipe.sections?.flatMap(section =>
-      section.components?.map(c => c.raw_text) || []
-    ) || [],
-    instructions: recipe.instructions?.map(step => step.display_text) || [],
-    video: recipe.youtube_url || recipe.original_video_url || recipe.video_url || null
-  };
-}
-
-/* Returns a map of the normalized response to display */ 
-export function normalizeTastyResponse(apiResponse) {
-  if (!apiResponse?.results) return [];
-
-  return apiResponse.results.map(normalizeTastyRecipe);
 }
